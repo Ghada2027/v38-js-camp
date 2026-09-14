f0dad15592d5d71c27c4f0ff99aa4ec540744b6c
Ghada <ghada.daher.79@gmail.com>
2026-09-14 20:25:23 +0200

    feat: introduce const/let and basic types

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-console.log("Nord Glow");
-console.log("Nord Glow");
-console.log("Nord Glow");
+const eventName = "Stars Live";
+
+console.log(eventName);
+console.log(eventName);
+
+let ticketCount = 0;
+
+console.log(ticketCount);
+
+ticketCount = 10;
+
+console.log(ticketCount);
+
+const isVip = false;
+
+console.log(isVip);
